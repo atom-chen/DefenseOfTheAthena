8c74a23a592e974c6a079e225116800e571f7e7b
ming0527 <67516569@qq.com>
2020-07-03 22:30:48 +0800

    test

--- proto/gen-go.bat
+++ proto/gen-go.bat
@@ -1,2 +1,2 @@
-protoc -I=./proto --proto_path=.\proto --gofast_out=.\pb_go *.proto
+protoc -I=./proto --proto_path=D:\WorkSpace\DefenseOfTheAthena\proto\proto --gofast_out=D:\WorkSpace\DefenseOfTheAthena\proto\pb_go *.proto
 pause
