fc39b89aa25d3408392bb722d1377c84db5a2525
ming0527 <67516569@qq.com>
2020-07-04 01:43:21 +0800

    [F]生成.d.ts的bug

--- proto/点击生成pb_ts.bat
+++ proto/点击生成pb_ts.bat
@@ -1,4 +1,4 @@
 @echo off
 start /b pbjs -t static-module -w commonjs -o ./pb_ts/proto.js *.proto 
-start /b pbts -o ./pb_ts/proto.ts ./pb_ts/proto.js 
+start /b pbts -o ./pb_ts/proto.d.ts ./pb_ts/proto.js 
 pause
